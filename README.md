8d74154af976576590c96def34186512db8de4dc
rubi <naiman.music.info@gmail.com>
2023-04-15 18:03:35 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# LinesOfLang - LoL
+LinesOfLang - also called LoL - is a cli tool that tells you the how much lines of code of (a) language(s) you have written inside a specified path (including subfolders). 
+
+## Usage
+The default argument ```-e``` or ```--extensions``` gives you the languages shown inside the [langs.toml](./src/langs.toml). If you don't see your language please add it to the [langs.toml](./src/langs.toml) in order to use the default value of extensions. If you execute the following command path argument will take the current working directory as its value:
+```
+python LoL.py
+```
+This will generate the following output:
+```
+path                lines of code  found languages    found extensions
+----------------  ---------------  -----------------  ------------------
+C:\repos\LoL\src               96  ['Python']         ['.py']
+```
+
+If you want to search inside a folder other than your current working directory then feel free to use the ```-p``` or ```--path``` argument:
+```
+python LoL.py -p C:\repos\TomlForge
+```
+This will produce the following output:
+```
+path                   lines of code  found languages    found extensions
+-------------------  ---------------  -----------------  ------------------
+C:\repos\TomlForge\               26  ['PowerShell']     ['.ps1']
+```
+
+If there is there is a specific file extension you are searching for just use the ```-e``` or ```--extensions``` argument:
+```
+path                   lines of code  found languages    found extensions
+-------------------  ---------------  -----------------  ------------------
+C:\repos\TomlForge\               26  []                 ['ps1']
+```
+
+As you can see he language does not get added to the found languages column. I am currently working on fixing this small issue. In the future you will also be able to load your own [langs.toml](./src/langs.toml) file with ```-l``` or ```--load``` but for now you'll have to rename the current one and replace it with your own.
---
+++ src/LoL.py
@@ -0,0 +1,96 @@
+from argparse import ArgumentParser, Namespace
+from os import getcwd, walk, path
+import tabulate
+import toml
+
+def get_default_extensions() -> list:
+    with open("langs.toml") as f:
+        data = toml.load(f)
+
+    default_extensions = []
+    for lang in data:
+        default_extensions.extend(data[lang]["extensions"])
+
+    return default_extensions
+
+parser = ArgumentParser(description="loc.py is used to get the lines of code inside a directory")
+
+# add path and languages arguments
+parser.add_argument("-p", "--path",
+                    metavar="path",
+                    type=str,
+                    help="specifies the path to the directory",
+                    default=f"{getcwd()}")
+
+parser.add_argument("-e", "--extensions",
+                    metavar="extensions",
+                    nargs="+", type=str,
+                    help="specifies the file extensions that should be searched",
+                    default=get_default_extensions())
+
+# store arguments to variables
+args: Namespace = parser.parse_args()
+arg_path = args.path
+arg_extensions = args.extensions
+
+# create code class
+class Code:
+    def __init__(self, path: str, extensions: str) -> None:
+        self.path = path
+        self.extensions = extensions
+        self.files = []
+        self.num_of_lines = 0
+        self.found_extensions = []
+        self.found_languages = []
+
+    def get_files(self):
+        for root, dirs, files in walk(self.path):
+            for name in files:
+                for extension in self.extensions:
+                    if name.endswith(extension):
+                        file_path = path.join(root, name)
+                        self.files.append(file_path)
+
+                        if extension not in self.found_extensions:
+                            self.found_extensions.append(extension)
+
+    def count_lines(self):
+        count = 0
+        for file in self.files:
+            with open (file,"r", encoding="utf-8", errors="ignore") as f:
+                count += len(f.readlines())
+
+        self.num_of_lines = count
+
+    def get_found_languages(self):
+        found_languages = []
+        with open("langs.toml") as f:
+            data = toml.load(f)
+
+        for lang in data:
+            for extension in data[lang]["extensions"]:
+                if extension in self.found_extensions:
+                    self.found_languages.append(lang)
+                    break
+
+    def output(self):
+        output_msg = [
+            [self.path, self.num_of_lines, self.found_languages , self.found_extensions]
+        ]
+
+        print(tabulate.tabulate(output_msg,
+                                headers=["path",
+                                         "lines of code",
+                                         "found languages",
+                                         "found extensions"]))
+
+# create Code object
+code_obj = Code(arg_path, arg_extensions)
+
+# get files and lines of code number
+code_obj.get_files()
+code_obj.count_lines()
+code_obj.get_found_languages()
+
+# output
+code_obj.output()
